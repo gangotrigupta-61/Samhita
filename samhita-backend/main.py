c81af33f03f4857a18dac07745d3bc5a2075c451
gangotrigupta-61 <gangotrigupta61@gmail.com>
2026-04-03 09:08:28 +0530

    FixedCORS

--- samhita-backend/main.py
+++ samhita-backend/main.py
@@ -27,7 +27,7 @@ app = FastAPI(
 # CORS — allow frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "*"],
+    allow_origins=["https://samhita-mjvo.vercel.app/", "https://samhita-gh4f.onrender.com/", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
